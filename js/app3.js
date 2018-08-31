7841b53ad087750c106ea742bfac64b696799180
Ramona Saintandre <thenewmona@gmail.com>
2018-08-30 23:00:56 -0400

    timer is now display needs help, need to figure out where to stop

--- js/app3.js
+++ js/app3.js
@@ -21,7 +21,9 @@ function flipCard() {
     if (!hasFlippedCard) { //first click !
         hasFlippedCard = true;
         firstCard = this;
+        startTimer();
         return;
+        
     }
 
     //second click        
@@ -40,16 +42,17 @@ function flipCard() {
 // using the data attribute within the HTML to do this 
 function checkForMatch() {
     let isMatch = firstCard.dataset.card === secondCard.dataset.card; //this is if its a match
-    startTimer();
+
 
     //test if the cards match
     //console.log(firstCard.dataset.card);
     //console.log(secondCard.dataset.card);
     //cleaned up the code using a ternary operator 
     //if they match 
-    isMatch ? disableCards() : unflipCards();
+    isMatch ? disableCards() : unflipCards();    
+        //startTimer();
+    }
 
-}
 
 function disableCards() {
     firstCard.removeEventListener('click', flipCard); //remove the eventListener if it's a match. You have to add the event and the function that you called 
@@ -68,22 +71,20 @@ function unflipCards() {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
         resetCard();
-        
+
     }, 700);
 }
-
 
 function resetCard() {
     [hasFlippedCard, lockBoard] = [false, false];
     [firstCard, secondCard] = [null, null];
-
 }
 
 (function shuffle() {
     cards.forEach(card => {
         let randomPos = Math.floor(Math.random() * 16);
         card.style.order = randomPos;
-            });
+    });
 })();
 
 let moves = 0;
@@ -95,14 +96,13 @@ function addMove() {
 }
 if (hasFlippedCard.length === 2) {
     //checkForMatch(clickTarget);
-    
-}
 
+}
 
 function removeStars() {
     if (moves === 10 || moves === 20 || moves === 30) {
         removeStar();
-        
+
     }
 }
 
@@ -112,51 +112,59 @@ function removeStar() {
         if (stars.style.display !== 'none') {
             stars.style.display = 'none';
             break; //need to find out what break does 
-            
+
         }
     }
 }
 removeStars();
 
-//timer https://www.w3schools.com/js/js_timing.asp
-
-//show timer
-let time = 0;
-let timer;
-let minutes;
-let seconds;
-
-// let timerNr = true; //timerNotRunning
-// let timerOff = true;
+//timer 
+let stopTimer = false;//need this for the timer and the reset 
+let resetGame = true; //need this for game reset and modal 
 
 function showTimer() {
     let timer = document.querySelector('.timer');
-    let minutes = Math.floor(time / 60);
-    let seconds = Math.floor(time % 60);
+    // let minutes = Math.floor(time / 60);
+    // let seconds = Math.floor(time % 60);
     //console.log(timer);
     timer.innerHTML = minutes + ':' + seconds;
 }
-//start timer 
+
 function startTimer() {
-    timeStart = setInterval(() => { //setInterval repeats execution of the funtion
-        time++;
-        showTimer();
-       // console.log(time);
-    }, 1000);
+    if (resetGame == true) {        
+        let timer = 0;
+        let hour = 0;
+        let minute = 0;
+        let second = 0;
+        timeStart = setInterval(() => {//8-30 if i use => timer works, change it per https://www.w3schools.com/js/js_timing.asp it acts like it wants to start but doesnt
+            ++timer;
+            hour = Math.floor(timer / 3600);
+            minute = Math.floor((timer - hour * 3600) / 60);
+            second = timer - hour * 3600 - minute * 60;
+            if (hour < 10) hour = '0' + hour;
+            if (minute < 10) minute = '0' + second;
+            document.querySelector(".timer").innerHTML = hour + ':' + minute + ':' + second;
+            if (stopTimer) {
+                document.querySelector(".timer").innerHTML = "00:00";
+                timer = 0;
+                hour = 0;
+                minute = 0;
+                second = 0;
+                return;
+            showTimer();
+            }
+        },1000);
+    }
 }
 
-// //startTimer();
-// if (timerNr) {
-//     startTimer();
-//     timerNr = true;
+// function stopTimer() {
+//     clearInterval(timeStart); //clearInterval needs to use the variable from the setInterval 
 // }
-
-//stopping the timer
-
-function stopTimer() {
-    clearInterval(timeStart); //clearInterval needs to use the variable from the setInterval 
-}
 //stopTimer();
+
+/*
+ * Stop the game timer and return seconds elapsed
+ */
 
 
 //reset game - reset clock - moves -stars- reshuffle the cards 
@@ -171,3 +179,4 @@ function stopTimer() {
 
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+//stopTimer = true;
